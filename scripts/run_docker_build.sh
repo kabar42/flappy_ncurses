c7818695f6425af6dcbce33a89923dcddcca1ef6
Kent Barnett <barnettkent@gmail.com>
2016-12-17 15:39:26 -0600

    Remove containers when running the build script

--- scripts/run_docker_build.sh
+++ scripts/run_docker_build.sh
@@ -3,4 +3,7 @@
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 REPO_DIR="$(dirname ${SCRIPT_DIR})"
 
-docker run -it -v ${REPO_DIR}:/data buildbox
+BOXNAME=buildbox
+
+docker build -t ${BOXNAME} ${REPO_DIR}
+docker run --rm -it -v ${REPO_DIR}:/data ${BOXNAME}
