c51fe1a1f5991d5badfeade9e4424b74c8d49c0b
Kent Barnett <barnettkent@gmail.com>
2016-12-18 16:24:29 -0600

    Add options to the build script

--- scripts/run_docker_build.sh
+++ scripts/run_docker_build.sh
@@ -3,7 +3,30 @@
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 REPO_DIR="$(dirname ${SCRIPT_DIR})"
 
-BOXNAME=buildbox
+function usage {
+cat <<-EOF
+Run a build in the docker container
+USAGE: run_docker_build [options]
+OPTIONS:
+    -b      Build the docker image before using it
+EOF
+}
 
-docker build -t ${BOXNAME} ${REPO_DIR}
+BOXNAME=buildbox
+BUILD_IT=false
+
+case "$1" in
+    "")
+    ;;
+    -b)
+    BUILD_IT=true
+    ;;
+    *)
+    usage && exit 0
+    ;;
+esac
+    
+if [ $BUILD_IT == true ]; then
+    docker build -t ${BOXNAME} ${REPO_DIR}
+fi
 docker run --rm -it -v ${REPO_DIR}:/data ${BOXNAME}
