4230402cc095b80f1acef6555660150b1b609240
Kent Barnett <barnettkent@gmail.com>
2016-12-24 10:04:41 -0600

    Use -> syntax to improve readability

--- src/bird.c
+++ src/bird.c
@@ -2,65 +2,65 @@
 
 void initPlayer(Bird_T* player)
 {
-    (*player).xPos = (COLS / 4);
-    (*player).yPos = (LINES / 2);
-    (*player).speed = 0;
-    (*player).posInBlock = (BLOCK_HEIGHT / 2);
-    (*player).dispChar = 'W';
-    (*player).colorPair = 1;
-    (*player).dead = false;
-    (*player).window = newwin(1, 1, (*player).yPos, (*player).xPos);
+    player->xPos = (COLS / 4);
+    player->yPos = (LINES / 2);
+    player->speed = 0;
+    player->posInBlock = (BLOCK_HEIGHT / 2);
+    player->dispChar = 'W';
+    player->colorPair = 1;
+    player->dead = false;
+    player->window = newwin(1, 1, player->yPos, player->xPos);
 
-    wattrset((*player).window, COLOR_PAIR((*player).colorPair));
-    mvwaddch((*player).window, 0, 0, (*player).dispChar);
+    wattrset(player->window, COLOR_PAIR(player->colorPair));
+    mvwaddch(player->window, 0, 0, player->dispChar);
 }
 
 void updatePlayer(Bird_T* player)
 {
-    (*player).speed--;
-    (*player).posInBlock = (*player).posInBlock + (*player).speed;
+    player->speed--;
+    player->posInBlock = player->posInBlock + player->speed;
 
     // Check if the player block should move down
-    if((*player).speed < 0 && (*player).posInBlock < 0)
+    if(player->speed < 0 && player->posInBlock < 0)
     {
-        int change = -((*player).posInBlock);
-        (*player).yPos = (*player).yPos + (change / BLOCK_HEIGHT) + 1;
-        (*player).posInBlock = BLOCK_HEIGHT - (change % BLOCK_HEIGHT); 
+        int change = -(player->posInBlock);
+        player->yPos = player->yPos + (change / BLOCK_HEIGHT) + 1;
+        player->posInBlock = BLOCK_HEIGHT - (change % BLOCK_HEIGHT); 
     }
     // Check if the player block should move up
-    else if((*player).speed > 0 && (*player).posInBlock > BLOCK_HEIGHT)
+    else if(player->speed > 0 && player->posInBlock > BLOCK_HEIGHT)
     {
-        int change = (*player).posInBlock - BLOCK_HEIGHT;
-        (*player).yPos = (*player).yPos - (change / BLOCK_HEIGHT) - 1;
-        (*player).posInBlock = change % BLOCK_HEIGHT; 
+        int change = player->posInBlock - BLOCK_HEIGHT;
+        player->yPos = player->yPos - (change / BLOCK_HEIGHT) - 1;
+        player->posInBlock = change % BLOCK_HEIGHT; 
     }
     checkBounds(player);
 }
 
 void jumpPlayer(Bird_T* player)
 {
-    (*player).speed = BLOCK_HEIGHT;
+    player->speed = BLOCK_HEIGHT;
 }
 
 void drawPlayer(Bird_T* player)
 {
-    mvwin((*player).window, (*player).yPos, (*player).xPos);
-    wnoutrefresh((*player).window);
+    mvwin(player->window, player->yPos, player->xPos);
+    wnoutrefresh(player->window);
 }
 
 static void checkBounds(Bird_T* player)
 {
-    if((*player).yPos < 0)
+    if(player->yPos < 0)
     {
-        (*player).yPos = 0;
+        player->yPos = 0;
     }
-    if((*player).xPos < 0 )
+    if(player->xPos < 0 )
     {
-        (*player).xPos = 0;
+        player->xPos = 0;
     }
-    if((*player).xPos > COLS)
+    if(player->xPos > COLS)
     {
-        (*player).xPos = COLS;
+        player->xPos = COLS;
     }
 }
 
--- src/pipe.c
+++ src/pipe.c
@@ -11,36 +11,36 @@ void initPipe(Pipe_T* pipe)
     int gapPos = (rand() % (PIPE_MAX_GAP_POS - PIPE_MIN_GAP_POS)) + PIPE_MIN_GAP_POS;
     int gapSize = (rand() % (PIPE_MAX_GAP_SIZE - PIPE_MIN_GAP_SIZE)) + PIPE_MIN_GAP_SIZE;
 
-    (*pipe).position = COLS;
-    (*pipe).width = PIPE_WIDTH;
-    (*pipe).gapPos = gapPos;
-    (*pipe).gapSize = gapSize;
-    (*pipe).posInBlock = PIPE_BLOCK_WIDTH;
-    (*pipe).topPipe = newwin(((*pipe).gapPos-1),
+    pipe->position = COLS;
+    pipe->width = PIPE_WIDTH;
+    pipe->gapPos = gapPos;
+    pipe->gapSize = gapSize;
+    pipe->posInBlock = PIPE_BLOCK_WIDTH;
+    pipe->topPipe = newwin((pipe->gapPos-1),
                              PIPE_WIDTH,
                              0,
-                             (*pipe).position);
-    (*pipe).bottomPipe = newwin(0,
+                             pipe->position);
+    pipe->bottomPipe = newwin(0,
                                 PIPE_WIDTH,
-                                ((*pipe).gapPos + (*pipe).gapSize + 1),
-                                (*pipe).position);
-    (*pipe).shouldReap = false;
+                                (pipe->gapPos + pipe->gapSize + 1),
+                                pipe->position);
+    pipe->shouldReap = false;
 
-    fillInPipeSegment((*pipe).topPipe);
-    fillInPipeSegment((*pipe).bottomPipe);
+    fillInPipeSegment(pipe->topPipe);
+    fillInPipeSegment(pipe->bottomPipe);
 }
 
 void updatePipe(Pipe_T* pipe, int speed)
 {
-    (*pipe).posInBlock = (*pipe).posInBlock - speed;
-    if((*pipe).posInBlock < 0)
+    pipe->posInBlock = pipe->posInBlock - speed;
+    if(pipe->posInBlock < 0)
     {
-        int change = -((*pipe).posInBlock);
-        (*pipe).position = (*pipe).position - (change / PIPE_BLOCK_WIDTH) - 1;
-        (*pipe).posInBlock = PIPE_BLOCK_WIDTH - (change % PIPE_BLOCK_WIDTH); 
+        int change = -(pipe->posInBlock);
+        pipe->position = pipe->position - (change / PIPE_BLOCK_WIDTH) - 1;
+        pipe->posInBlock = PIPE_BLOCK_WIDTH - (change % PIPE_BLOCK_WIDTH); 
     }
 
-    if((*pipe).position < (-PIPE_WIDTH))
+    if(pipe->position < (-PIPE_WIDTH))
     {
         cleanupPipe(pipe);
     }
@@ -48,10 +48,10 @@ void updatePipe(Pipe_T* pipe, int speed)
 
 void drawPipe(Pipe_T* pipe)
 {
-    mvwin((*pipe).topPipe, 0, (*pipe).position);
-    mvwin((*pipe).bottomPipe, ((*pipe).gapPos + (*pipe).gapSize + 1), (*pipe).position);
-    wnoutrefresh((*pipe).topPipe);
-    wnoutrefresh((*pipe).bottomPipe);
+    mvwin(pipe->topPipe, 0, pipe->position);
+    mvwin(pipe->bottomPipe, (pipe->gapPos + pipe->gapSize + 1), pipe->position);
+    wnoutrefresh(pipe->topPipe);
+    wnoutrefresh(pipe->bottomPipe);
 }
 
 static void fillInPipeSegment(WINDOW* pipeWin)
@@ -71,8 +71,8 @@ static void fillInPipeSegment(WINDOW* pipeWin)
 
 static void cleanupPipe(Pipe_T* pipe)
 {
-    (void) delwin((*pipe).topPipe);
-    (void) delwin((*pipe).bottomPipe);
-    (*pipe).shouldReap = true;
+    (void) delwin(pipe->topPipe);
+    (void) delwin(pipe->bottomPipe);
+    pipe->shouldReap = true;
 }
 
